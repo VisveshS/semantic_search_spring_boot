3d62984109d6f4ffd4f41a39f959ffac199eab25
visvesh <visveshsubra@gmail.com>
2026-06-05 17:58:11 +0530

    clustering feature added

--- src/main/java/com/example/demo/ViewController.java
+++ src/main/java/com/example/demo/ViewController.java
@@ -4,31 +4,23 @@ import com.example.demo.model.QueryResultSlice;
 import com.example.demo.model.Semantic_search;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import java.io.IOException;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Deque;
 
 @Controller
 public class ViewController {
 
     Semantic_search semantic_search = new Semantic_search();
 
-    @GetMapping("/home")
+    @GetMapping("/index")
     public String viewForm(Model model) {
         model.addAttribute("formstate", new FormState());
-        return "form";
-    }
-
-    @PostMapping("/index")
-    public String viewForm(@RequestParam Integer useraction, @RequestParam String args, Model model) {
         ArrayList<QueryResultSlice> output = null;
         try {
-            output = semantic_search.query(useraction, args);
+            output = semantic_search.query(9, "3 10");
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
@@ -36,6 +28,24 @@ public class ViewController {
             System.out.println(e.getMessage());
         }
         model.addAttribute("query_result", output);
+        return "form";
+    }
+
+    @PostMapping("/index")
+    public String viewForm(@RequestParam Integer useraction, @RequestParam String args, Model model) {
+        ArrayList<QueryResultSlice> output = null;
+        ArrayList<QueryResultSlice> privateNotesTags = null;
+        try {
+            output = semantic_search.query(useraction, args);
+            privateNotesTags = semantic_search.GetPrivateNotesTags();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+        model.addAttribute("query_result", output);
+        model.addAttribute("privatenodetags", privateNotesTags);
         model.addAttribute("new_entries", semantic_search.newEntries);
         return "form";
     }
--- src/main/java/com/example/demo/model/Semantic_search.java
+++ src/main/java/com/example/demo/model/Semantic_search.java
@@ -1,7 +1,5 @@
 package com.example.demo.model;
 
-import com.example.demo.model.QueryResultSlice;
-
 import java.util.*;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
@@ -19,7 +17,9 @@ public class Semantic_search {
 
     Boolean constructed = false;
     ArrayList<QueryResultSlice> queryoutput = new ArrayList<>();
+    ArrayList<QueryResultSlice> privateNotesTags = new ArrayList<>();
     public ArrayList<String> newEntries = new ArrayList<String>();
+    public ArrayList<String> persist = new ArrayList<String>(); //url from bulk insertion
 
     BufferedReader reader;
     PrintWriter writer;
@@ -47,6 +47,8 @@ public class Semantic_search {
             throw new RuntimeException(e);
         }
         constructed = true;
+        for(int i=0;i<10;i++)
+            persist.add("");
     }
 
     //if qurey string size = 0 then setup embeddings , if = 1 then run
@@ -75,7 +77,7 @@ public class Semantic_search {
                 String cansave = reader.readLine();
                 String num_embeddings = reader.readLine();
                 queryoutput.clear();
-                queryoutput.add(new QueryResultSlice(new ArrayList<>(List.of(args, dburl, dbdesc,nodeid_in_knn_count, pagerank, clustering, cansave, QueryResultSlice.fetchSourceSite(dburl), num_embeddings))));
+                queryoutput.add(new QueryResultSlice(new ArrayList<>(List.of(args, dburl, dbdesc,nodeid_in_knn_count, pagerank, clustering, cansave, QueryResultSlice.fetchSourceSite(dburl), num_embeddings, persist.get(0)))));
                 line = reader.readLine();
                 nline = Integer.parseInt(line);
                 for(int i=0;i<nline;i++) {
@@ -84,6 +86,16 @@ public class Semantic_search {
                     line = reader.readLine();
                     String[] tokens = line.split(" ");
                     queryoutput.add(new QueryResultSlice(new ArrayList<>(List.of(tokens[0], dburl, dbdesc, tokens[1],tokens[2],String.valueOf(mutual_knn.charAt(i)),QueryResultSlice.fetchSourceSite(dburl)))));
+                }
+                line = reader.readLine();
+                nline = Integer.parseInt(line);
+                privateNotesTags.clear();
+                System.out.println(line);
+                for(int i=0;i<nline;i++) {
+                    String label = reader.readLine();
+                    String score = reader.readLine();
+                    System.out.println(label);
+                    privateNotesTags.add(new QueryResultSlice(new ArrayList<>(List.of(label, score))));
                 }
             }
             catch (IOException e) {
@@ -102,7 +114,7 @@ public class Semantic_search {
                 String num_embeddings = reader.readLine();
                 line = reader.readLine();
                 nline = Integer.parseInt(line);
-                queryoutput.add(new QueryResultSlice(new ArrayList<>(List.of("", "", args,"","", cscore, cansave,"", num_embeddings))));
+                queryoutput.add(new QueryResultSlice(new ArrayList<>(List.of("", "", args,"","", cscore, cansave,"", num_embeddings, persist.get(0)))));
                 for(int i=0;i<nline;i++) {
                     dburl = reader.readLine();
                     dbdesc = reader.readLine();
@@ -110,6 +122,14 @@ public class Semantic_search {
                     String[] tokens = line.split(" ");
                     Integer nid = Integer.parseInt(tokens[0]);
                     queryoutput.add(new QueryResultSlice(new ArrayList<>(List.of(tokens[0], dburl, dbdesc,tokens[1],tokens[2],String.valueOf(mutual_knn.charAt(i)),QueryResultSlice.fetchSourceSite(dburl)))));
+                }
+                line = reader.readLine();
+                nline = Integer.parseInt(line);
+                privateNotesTags.clear();
+                for(int i=0;i<nline;i++) {
+                    String label = reader.readLine();
+                    String score = reader.readLine();
+                    privateNotesTags.add(new QueryResultSlice(new ArrayList<>(List.of(label, score))));
                 }
             }
             catch (IOException e) {
@@ -130,10 +150,11 @@ public class Semantic_search {
             String cansave = reader.readLine();
             String insertfield[] = args.split(" ",2);
             String num_embeddings = String.valueOf(Integer.parseInt(queryoutput.get(0).fetchSlice().get(8))+1);
-            queryoutput.set(0,new QueryResultSlice(new ArrayList<>(List.of(new_index.toString(), insertfield[0], insertfield[1], nodeid_in_knn_count,pagerank,cscore,cansave,QueryResultSlice.fetchSourceSite(insertfield[0]), num_embeddings, num_embeddings))));
+            queryoutput.set(0,new QueryResultSlice(new ArrayList<>(List.of(new_index.toString(), insertfield[0], insertfield[1], nodeid_in_knn_count,pagerank,cscore,cansave,QueryResultSlice.fetchSourceSite(insertfield[0]), num_embeddings, persist.get(0)))));
             for(int i=1;i<queryoutput.size();i++) {
                 queryoutput.get(i).fetchSlice().set(5,String.valueOf(mutual_knn.charAt(i-1)));
             }
+            persist.set(0,"");
         }
 //    output[0] = (int id, string url, string description, int in_knn_length, float pagerank, float triangle_mesh_fraction, cansave) [details of_node]
 //    output[1:] = (int id, string url, string description, int in_knn_length, float pagerank, mutual knn)
@@ -193,9 +214,31 @@ public class Semantic_search {
             String url = newEntry[0];
             String desc = newEntry[1];
             newEntries.remove(Integer.parseInt(args));
+            persist.set(0,url);
             this.query(1, desc);
-            queryoutput.get(0).fetchSlice().add(url);
+        }
+        else if (action == 9) {
+            queryoutput.clear();
+            writer.println("cluster");
+            String clustertuning[] = args.split(" ",2);
+            writer.println(clustertuning[0]);//numhops
+            writer.println(clustertuning[1]);//numneigh
+            String cansave = reader.readLine();
+            String num_embeddings = reader.readLine();
+            line = reader.readLine();
+            nline = Integer.parseInt(line);
+            queryoutput.add(new QueryResultSlice(new ArrayList<>(List.of("-1", "", args,"","", "0", cansave,"", num_embeddings, persist.get(0)))));
+            for(int i=0;i<nline;i++) {
+                dburl = reader.readLine();
+                dbdesc = reader.readLine();
+                line = reader.readLine();
+                String[] tokens = line.split(" ");
+                queryoutput.add(new QueryResultSlice(new ArrayList<>(List.of(tokens[0], dburl, dbdesc,tokens[1],tokens[2],"0",QueryResultSlice.fetchSourceSite(dburl), tokens[3]))));
+            }
         }
         return queryoutput;
+    }
+    public ArrayList<QueryResultSlice> GetPrivateNotesTags() {
+        return privateNotesTags;
     }
 }
